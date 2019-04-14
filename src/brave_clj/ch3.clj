b79c5bcf7510141d78e06350f4c3465f7bd41964
Binay Mishra <binaymishramca@gmail.com>
2019-04-14 16:27:05 +0530

    and / or operator

--- src/brave_clj/ch3.clj
+++ src/brave_clj/ch3.clj
@@ -44,6 +44,68 @@
 (if false
 	"By Zeus's hammer!"
 	"By Aquaman's trident!")
-	
+
+
+;;do 
+;; The do operator lets you wrap up multiple forms in parentheses and runeach of them.
+
+(if true
+	(do (println "Success!")
+		"By Zeus's hammer!")
+	(do (println "Failure!")
+	"By Aquaman's trident!"))
+
+;; when
+;; The when operator is like a combination of if and do
+
+(when true
+	(println "Success!")
+	"abra cadabra")
+
+
+(nil? 1) 
+;; false, because 1 evaluates to 1 which not nil
+
+(nil? nil)
+;; true, because nil is nil
+
+(if "Binay Mishra"
+	"My name is Binay Mishra")
+; => "My name is Binay Mishra"
+; any value in clojure is evaluates to itself value and if successfully evaluated, than it is true
+
+(if nil
+	("This part will not be returned because nil is false")
+	"nil is false")
+; => "nil is false"
+
+;; or returns either the first truthy value or the last value
+(or false nil :large_I_mean_venti :why_cant_I_just_say_large)
+; large_I_mean_venti value got returned because it is the first truthly value encounted in the list. After false and nil
+
+(or 
+	(= 0 1)
+	(= "yes" "no"))
+
+; false
+; (= 0 1) evaluates to false , (= "yes" "no") evaluates to false; 
+; (or false false) the false will be returned because it is the last value
+
+(or nil)
+; nil
+; not other truthy value found, hence nil just one value is the last value in secquence returned.
+
+;; and returns the first falsey value or, if no values are falsey, the last truthy value
+(and :free_wifi :hot_coffe)
+; :hot_coffe
+
+; :free_wifi and :hot_coffe is truthy values, so according to and it returned the last truthy value,
+; because there is no falsely value in the secquence
+
+ (and :feeling_super_cool nil false)
+ ; nil
+ ; Here the nil is return that is the first falsey value encounted. so no need to evaluate any further.
+
+
 ;;(use 'brave-clj.ch3 :reload-all)
 (println "Successfully re-loaded all the forms !!")
